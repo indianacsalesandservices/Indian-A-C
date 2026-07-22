4a76b25ece17299eaeb0caf9eed0a001c140ffd1
indianacsalesandservices <indianacsalesandservices@users.noreply.github.com>
2026-07-22 15:06:44 +0530

    Remove WordPress folder

--- wordpress/theme/footer.php
+++
@@ -1,8 +0,0 @@
-  <footer class="site-footer">
-    <div class="container">
-      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All rights reserved.</p>
-    </div>
-  </footer>
-  <?php wp_footer(); ?>
-</body>
-</html>
